607f42db7bac3a0cff419c88473753b7e2d2cc86
tikam321 <tikamsuvasiya333@gmail.com>
2026-03-20 19:20:14 +0530

    Jira-ops-agent- added the secret logic for jira credentials

--- src/main/java/com/jiraops/agent/JiraOpsAgentApplication.java
+++ src/main/java/com/jiraops/agent/JiraOpsAgentApplication.java
@@ -1,5 +1,6 @@
 package com.jiraops.agent;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -9,6 +10,9 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class JiraOpsAgentApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().forEach(e ->
+                System.setProperty(e.getKey(), e.getValue()));
         SpringApplication.run(JiraOpsAgentApplication.class, args);
     }
 }
